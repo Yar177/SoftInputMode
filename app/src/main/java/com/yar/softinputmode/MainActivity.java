13cfa09784af1b76a7e68d1ce5755f70d7af3773
Yar177 <yarsher.dev@gmail.com>
2019-06-07 08:03:41 -0400

    webView

--- app/src/main/java/com/yar/softinputmode/MainActivity.java
+++ app/src/main/java/com/yar/softinputmode/MainActivity.java
@@ -1,13 +1,54 @@
 package com.yar.softinputmode;
 
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webview);
+        button = (Button) findViewById(R.id.button);
+
+//
+
+    }
+
+    public void signUp(View view) {
+        String url = "http://qa.excellusbcbs.com/wps/myportal/xl/member/registration";
+
+        webView.setVisibility(View.VISIBLE);
+        button.setVisibility(View.INVISIBLE);
+        webView.setWebViewClient(new myWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+
+    }
+
+
+}
+
+class myWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+
+    @Override
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        handler.proceed(); // Ignore SSL certificate errors
     }
 }
